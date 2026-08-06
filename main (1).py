85425e4eefe0e834998e9d70f93a95628b8ebb1b
sdmupas2031-beep <sdmupas2031@crc.pshs.edu.ph>
2026-08-06 09:31:28 +0800

    Added comments to the coordinate input

--- main (1).py
+++ main (1).py
@@ -1,8 +1,10 @@
 import math
 
-#get coordinates from the user
+# Ask the user to enter the coordinates of the first point
 x1= float(input("Enter x1:"))
 y1= float(input("Enter y1:"))
+
+# Ask the user to enter the coordinates of the second point
 x2= float(input("Enter x2:"))
 y2= float(input("Enter y2:"))
 
